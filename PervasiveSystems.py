4a8510ad675b0d403e614beb41351f94df5f622d
Roberto Falconi <falkons@outlook.com>
2018-03-26 14:24:10 +0200

    Update PervasiveSystems.py

--- PervasiveSystems.py
+++ PervasiveSystems.py
@@ -20,7 +20,7 @@
  * Modifications copyright (C) 2018 Roberto Falconi
  * LinkedIn: https://www.linkedin.com/in/robertofalconi95/
  * GitHub: https://github.com/RobertoFalconi95/HouseTemperatureMonitoring
- * Hackster Full description: https://www.hackster.io/Falkons/house-temperature-monitoring-with-aws-and-raspberry-3b6410
+ * Hackster Full description: https://www.hackster.io/Falkons/house-temperature-monitoring-with-aws-iot-and-raspberry-pi-3b6410
  * SlideShare presentation: https://www.slideshare.net/RobertoFalconi4/house-temperature-monitoring-with-amazon-web-services-and-raspberry
  * YouTube video: https://youtu.be/gQxOSbcN79s
  *
